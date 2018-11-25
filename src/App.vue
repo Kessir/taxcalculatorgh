ccbb8cc7eff4f001603ad3347c19bcac878ba85c
Kessir Adjaho <kessir.adjaho@cloudator.com>
2018-11-25 16:22:32 +0200

    Fixed css to match previous version

--- src/App.vue
+++ src/App.vue
@@ -110,10 +110,8 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
