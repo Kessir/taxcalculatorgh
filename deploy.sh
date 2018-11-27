d4f1f80c326dea2fe36f8e2937f0c8f81b71bf21
Kessir Adjaho <kessir.adjaho@cloudator.com>
2018-11-27 18:42:40 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f  https://github.com/Kessir/taxcalculatorgh.git master:gh-pages
 
 cd -
