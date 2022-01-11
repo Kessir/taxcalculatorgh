bb1299067953aeb84f6f4c0f80c61e54b113a9c0
Kessir <m.kessir.adjaho@gmail.com>
2022-01-11 08:25:28 +0100

    Updated tax rates in line with Act 1071

--- src/lib/rates.js
+++ src/lib/rates.js
@@ -1,12 +1,24 @@
 export const SSNIT_RATE = 5.5;
+
+// Effective from 01/01/2022
 export const monthlyTaxRates = [
-  [0, 319],
-  [5, 100],
-  [10, 120],
+  [0, 365],
+  [5, 110],
+  [10, 130],
   [17.5, 3000],
-  [25, 16461],
+  [25, 16395],
   [30, Number.POSITIVE_INFINITY] // anything above GHC 20,000
 ];
+
+// Effective until 31/12/2021
+// export const monthlyTaxRates = [
+//   [0, 319],
+//   [5, 100],
+//   [10, 120],
+//   [17.5, 3000],
+//   [25, 16461],
+//   [30, Number.POSITIVE_INFINITY] // anything above GHC 20,000
+// ];
 
 // OLD
 // export const monthlyTaxRates = [
